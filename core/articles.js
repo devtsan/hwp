6e2585e0606b22b1e5b798da9d668fca077573d2
devtsan <102000082+devtsan@users.noreply.github.com>
2022-03-31 20:37:37 +0630

    update articles.js

--- core/articles.js
+++ core/articles.js
@@ -1,1 +1,20 @@
-
+import { siteinfo } from "./config.js";
+let wpsite = siteinfo.site;
+(async () => {
+    async function getWPposts(wpsite) {
+        const url = `https://public-api.wordpress.com/rest/v1.1/sites/${wpsite}/posts/`;
+        const response = await fetch (url);
+        const awpp = await response.json();
+        return awpp.posts;
+    }
+    const articles = await getWPposts(wpsite);
+    
+    console.log(articles);
+    articles.forEach(
+        (article) => {
+            const articlesContainer = document.querySelector('#articles');
+            articlesContainer.innerHTML += <img src="${article.featured_image}" style="width:100%"/><p>No.<strong>${article.ID}</strong> ${article.title}</p><p>Published: ${article.date}, Modified: ${article.modified}</p>;
+            // console.log(No.${article.ID} ${article.title});
+        }
+    )
+}) ();
